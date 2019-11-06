b9d47b52ba6e2473406c7b801e03dd84a6315b2f
Hanna Lee <mani21na@gmail.com>
2019-11-06 19:53:32 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,14 +2,28 @@ class ApplicationController < Sinatra::Base
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
-    enable :sessions
-    set :session_secret, "password_security"
   end
 
-  # code actions here!
-  get '/'
-    erb :index
-  end 
+  ##loads index page 
+  get '/recipes'
+    @recipes = Recipes.all 
+    erb :index 
+  end
+
+  ##loads new form
+  get '/recipes/new' do #loads new form
+    erb :new
+  end
   
+  ##creates a recipe
+  post '/recipes' do
+    @recipe = Recipe.new(params)
+    redirect '/recipes/#{@recipe.id}'
+  end
   
+  ##loads show page
+  get '/recipes/:id' do 
+    @recipe = Recipe.find_by_id(params[:id])
+    erb :show
+  end
 end
